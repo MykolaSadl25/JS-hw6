c8b1f2b8b1cb328947224479e2d15a1c60861a11
Mykola Sadlii <mykolac08@gmail.com>
2025-10-03 19:34:03 +0100

    part 1

---
+++ js/loops.js
@@ -0,0 +1,25 @@
+// 1
+
+// 2
+for(let i = 2; i <= 20; i += 1){
+    if (i%2 === 0) {
+        console.log(i);
+    }
+}
+
+// 3
+
+// 4
+for(let i = 0; i <= 10; i += 1){
+    console.log(`7 x ${i} = ${7 * i}`);
+}
+
+const n = 10;
+for(let i = 0; i <= 18; i += 1){
+    if (i >= n) {
+        break;
+    }
+    console.log(i);
+}
+
+// 5
